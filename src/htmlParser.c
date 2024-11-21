5d855d5b7115c46586e20bd9390a741c27e8d366
CameronHumphreysGIT <Cam117version@gmail.com>
2024-11-21 18:48:57 -0500

    Workspace refactor, WIP

--- src/htmlParser.c
+++ src/htmlParser.c
@@ -1,4 +1,4 @@
-#include <htmlParser.h>
+#include <../include/htmlParser.h>
 
 char* parseRequest(char* request) {
     return "HTTP/1.1 200 OK\r\nContent-Length: 93\r\nContent-Type: text/html\r\n\r\n<html><body>Hi</body></html>";
